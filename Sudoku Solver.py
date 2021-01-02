7cadae5869c3e3f59cffa5cf18d3e1a53d4e5bde
Bolu Adubi <bolu.adubi@gmail.com>
2021-01-02 15:18:18 -0500

    added printGrid and solve functions to finish project

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,3 +1,6 @@
+import numpy as np
+import sys
+
 grid = [[5,3,0,0,7,0,0,0,0],
         [6,0,0,1,9,5,0,0,0],
         [0,9,8,0,0,0,0,6,0],
@@ -9,8 +12,8 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,0,8,0,0,7,9]]
 
 """
-x = x pos
-y = y pos
+x = x pos (row)
+y = y pos (column)
 n = number being checked
 """
 def isValid(x,y,n):
@@ -32,13 +35,42 @@ def isValid(x,y,n):
 
     for i in range(3):
         for j in range(3):
-            if grid[col + i][row + j] == n:
+            if grid[row + i][col + j] == n:
                 return False
 
     # if number passes all 3 checks, return true
     return True
 
+def printGrid(grid):
+    print(np.matrix(grid))
+
+def solve():
+    global grid
+
+    # find a blank cell (data in cell = 0)
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                # check if n(1-9) is a possible entry
+                # if number is valid, replace 0 with n  
+                # if number isn't valid, increase n by 1 (for n in range(1,10))
+                for n in range(1,10):
+                    if isValid(y,x,n):
+                        grid[y][x] = n # put valid number in
+                        solve() # call solve() recursively
+                        grid[y][x] = 0 # backtrace if needed (replace n with 0)
+                return
+
+    printGrid(grid)
+    input("Input anything to check if there are more solutions") # check if there are more solutions
+
+
 def main():
-    
+
+    print("Unsolved Sudoku Grid:\n")
+    printGrid(grid)
+
+    print("\nSolved Sudoku Grid:\n")
+    solve()
 
 main()
